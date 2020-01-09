a9631eae9164ba8681f96df2f69d8f4f4bb57fb7
raffaele <mzzrff@gmail.com>
2020-01-09 21:08:49 +0100

    comments

--- arduino_files/skinnerCapacitiveTest/skinnerCapacitiveTest.ino
+++ arduino_files/skinnerCapacitiveTest/skinnerCapacitiveTest.ino
@@ -1,31 +1,38 @@
+// This script is used to test capacitive buttons
+// and prints using serial port values detected by capacitive sensors
 
 #include <CapacitiveSensor.h>
 
 
-CapacitiveSensor   dx = CapacitiveSensor(3,4);       
-CapacitiveSensor   sx = CapacitiveSensor(2,5); 
+CapacitiveSensor   right = CapacitiveSensor(3,4);       
+CapacitiveSensor   left = CapacitiveSensor(2,5); 
 
-int buzzer=7;
+int buzzer=7; // buzzer pin number
+int tonehz = 200; 
+int threshold = 500;
 long val;
-void setup() {
-  // put your setup code here, to run once:
-  digitalWrite(12,HIGH);
-  pinMode(12,OUTPUT);
-Serial.begin(9600);
+
+void setup() 
+{
+  Serial.begin(9600); // initialize serial communication
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-val=dx.capacitiveSensor(10);
-Serial.println(val);
-if (val>500){
-  tone(buzzer, 200); // 
-  delay(100);
-  digitalWrite(12,LOW);
-}else{
-  noTone(buzzer);
-}
-delay(2);
-
+void loop() 
+{
+  // change between "left" and "right" sensor to print 
+  // values from the corrispondent button
+  val=right.capacitiveSensor(10);
+  Serial.println(val);
+  if (val>threshold){
+      tone(buzzer, tonehz); // if the value excedes a threshold the buzzer will produce a tone
+      delay(100);
+  }
+  else
+  {
+      noTone(buzzer);
+  }
+  
+  delay(2);
+  
 }
 
